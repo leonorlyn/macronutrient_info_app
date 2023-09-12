789e2ccc04db46863621504f299606f575ea78b3
lena <107754913+leonorlyn@users.noreply.github.com>
2023-09-12 13:23:25 -0700

    host revised

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,8 +26,7 @@ SECRET_KEY = "django-insecure-*6oh)r2l%$v5838cdyzf7(io(0q(*a=h)040(on+++0t3vggg(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
